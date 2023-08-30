44e9ad103647fb965517fab2cc854321231c169f
lyw <2354640364@qq.com>
2023-08-30 13:44:00 +0800

    join()

--- src/base/Solution.java
+++ src/base/Solution.java
@@ -10,5 +10,29 @@ public class Solution {
         runnableTask.start();
 
         System.out.println("main thread");
+
+
+
+        // test join():主线程等待子线程执行完毕再执行
+        Thread thread = new Thread(()->{
+            for(int i=0;i<10;i++) {
+                System.out.println("线程名=>"+Thread.currentThread().getName()+",i="+i);
+            }
+        });
+        thread.start();
+
+        for(int i=0;i<20;i++) {
+            System.out.println("线程名=>"+Thread.currentThread().getName()+",i="+i);
+            if(i==3) {
+                try {
+                    System.out.println("thread.join();开始");
+                    thread.join();
+                    System.out.println("thread.join();结束");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.println("end");
     }
 }
