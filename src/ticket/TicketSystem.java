d594b9437fade748c98da44675b3cc7fee4e3a12
lyw <2354640364@qq.com>
2023-08-30 15:47:33 +0800

    TicketSystem加锁

--- src/ticket/TicketSystem.java
+++ src/ticket/TicketSystem.java
@@ -1,14 +1,22 @@
 package ticket;
 
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class TicketSystem {
     private int amount;
+
+    private final ReentrantLock lock = new ReentrantLock(true);
 
     public TicketSystem(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * 1.sell()方法加关键字synchronized
+     * 2.尽量减小锁粒度：只对售票操作加锁
+     * 3.ReentrantLock
+     */
     public void sell() {
         if (amount <= 0) {
             throw new RuntimeException("没有票了！");
@@ -19,7 +27,19 @@ public class TicketSystem {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        amount--;
-        System.out.println(Thread.currentThread().getName() + "，剩余票数 => " + amount);
+
+        // this指代调用该方法的对象
+//        synchronized (this) {
+//            amount--;
+//            System.out.println(Thread.currentThread().getName() + "，剩余票数 => " + amount);
+//        }
+
+        lock.lock();
+        try{
+            amount--;
+            System.out.println(Thread.currentThread().getName() + "，剩余票数 => " + amount);
+        }finally {
+            lock.unlock();
+        }
     }
 }
